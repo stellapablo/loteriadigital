dc272add53b83a5666b082b1f21bd36958b170e1
Pablo <stella.pablo@gmail.com>
2017-08-16 21:34:06 +0000

    travis conf phpunit

---
+++ database/migrations/2017_08_16_180252_create_personal_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonalTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personal', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('dni');
+            $table->text('apellido');
+            $table->text('nombre');
+            $table->text('lugar');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('rrhh_documento', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('detalle')->nullable();
+            $table->integer('personal_id')->index();
+            $table->text('archivo')->nullable();
+            $table->integer('store_id')->index();
+            $table->integer('user_id')->index();
+            $table->integer('seccion_id')->index();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personal');
+
+        Schema::dropIfExists('rrhh_documento');
+
+    }
+}
---
+++ database/migrations/2017_08_16_180318_create_seccion_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSeccionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('secciones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre')->index();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('secciones');
+
+    }
+}
